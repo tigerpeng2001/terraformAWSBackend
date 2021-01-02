0425ba37303f69379a22e29de99f01f8d03e6593
Peng <tpeng@HIUSA129273.attlocal.net>
2021-01-02 14:39:31 -0600

    set S3 Bucket and objects not public

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
-provider "aws" {}
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
 
 module "data" {
   source = "./data"
@@ -18,14 +21,14 @@ resource "aws_dynamodb_table" "dynamodb-terraform-lock" {
   tags = merge(
     local.common_fix_tags,
     local.common_var_tags,
-    map (
-    "Name", "Terraform Lock Table"
+    map(
+      "Name", "Terraform Lock Table"
     )
   )
 }
 
 resource "aws_s3_bucket" "tfstate" {
-  bucket        = "terraform-states-${module.data.account_id}-${module.data.region}"
+  bucket        = "terraform-states-${module.data.account_id}"
   acl           = "private"
   force_destroy = "false"
 
@@ -44,11 +47,21 @@ resource "aws_s3_bucket" "tfstate" {
   tags = merge(
     local.common_fix_tags,
     local.common_var_tags,
-    map (
-    "Name", "tfstate bucket"
+    map(
+      "Name", "tfstate bucket"
     )
   )
 }
+
+resource "aws_s3_bucket_public_access_block" "tfstat-no-public-access" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
 
 output "tfstat_bucket_name" {
   value = "${aws_s3_bucket.tfstate.bucket}"
